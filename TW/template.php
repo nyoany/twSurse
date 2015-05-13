1c45360b74a0ea0286188f10e08b03bd7228c3db
nyoany <oana.nechiforescu@gmail.com>
2015-05-13 20:30:51 +0300

    Primul commit

---
+++ TW/template.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Ratsie Races</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+<div id= "banner"></div>
+<div id="main">
+<span id="page-title"><H1>Welcome Page</H1></span>
+ <form>
+
+</form> 
+<div id="footerI">
+<?php require 'footer.php';?>
+</div>
+</div>
+</body>
+</html> 
