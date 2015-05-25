91299237887db7f5a473b116a884997810453f2c
alex <biackalex@yahoo.com>
2015-05-25 16:36:00 +0300

    Added db scripts

---
+++ db/insert into races.sql
@@ -0,0 +1,13 @@
+/*
+-- Query: SELECT * FROM dball.races
+LIMIT 0, 1000
+
+-- Date: 2015-05-25 16:34
+*/
+INSERT INTO `races` (`id`,`date`,`participant1`,`participant2`,`winner`) VALUES (1,'2015-11-17 12:45:34',1,2,NULL);
+INSERT INTO `races` (`id`,`date`,`participant1`,`participant2`,`winner`) VALUES (2,'2015-11-17 12:45:34',2,3,NULL);
+INSERT INTO `races` (`id`,`date`,`participant1`,`participant2`,`winner`) VALUES (3,'2015-11-17 12:45:34',3,4,NULL);
+INSERT INTO `races` (`id`,`date`,`participant1`,`participant2`,`winner`) VALUES (4,'2015-11-17 12:45:34',4,5,NULL);
+INSERT INTO `races` (`id`,`date`,`participant1`,`participant2`,`winner`) VALUES (5,'2015-11-18 12:45:34',5,1,NULL);
+INSERT INTO `races` (`id`,`date`,`participant1`,`participant2`,`winner`) VALUES (6,'2013-11-18 12:45:34',2,3,2);
+INSERT INTO `races` (`id`,`date`,`participant1`,`participant2`,`winner`) VALUES (16,'2015-05-30 02:00:00',1,2,NULL);
---
+++ db/insert into rats.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM dball.rats
+LIMIT 0, 1000
+
+-- Date: 2015-05-25 16:34
+*/
+INSERT INTO `rats` (`id`,`name`,`wins`,`losses`,`cota`) VALUES (1,'Rat1',0,0,1.2);
+INSERT INTO `rats` (`id`,`name`,`wins`,`losses`,`cota`) VALUES (2,'Rat2',0,0,1.1);
+INSERT INTO `rats` (`id`,`name`,`wins`,`losses`,`cota`) VALUES (3,'Rat3',0,0,1.3);
+INSERT INTO `rats` (`id`,`name`,`wins`,`losses`,`cota`) VALUES (4,'Rat4',0,0,1.4);
+INSERT INTO `rats` (`id`,`name`,`wins`,`losses`,`cota`) VALUES (5,'Rat5',0,0,1.11);
