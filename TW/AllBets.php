4a7740e897a79bc3492b4f622b3cfec8fa8fd871
alex <biackalex@yahoo.com>
2015-06-02 16:13:01 +0300

    Display all Bets

---
+++ TW/AllBets.php
@@ -0,0 +1,70 @@
+<?php
+class AllBets{
+
+	function getAllBets(){
+	$conn = mysqli_connect('localhost','root','root','dball');
+	if (!$conn) {
+		die('Could not connect: ' . mysqli_error($conn));
+	}
+	$sql = " Select DATE_FORMAT(r.date,'%d-%m-%Y') as date, DATE_FORMAT(r.date,'%h:%i:00') as time, r1.name as r1, 
+	 (select count(ratID) from bets where ratId=r.participant1) as b1, r2.name as r2,
+	 (select count(ratID) from bets where ratId=r.participant2) as b2
+		 from races as r, rats as r1, rats as r2, bets as b
+		 where (r1.id = r.participant1) 
+		 and (r2.id = r.participant2);";
+	$index = 1;
+	$races = $conn->query($sql);
+	if ($races != null) {
+	if($races->num_rows > 0){
+		echo"<table id = 'dataTable' class='tabel'>
+						<thead>
+							<th>Crt</th>
+							<th>Date</th>
+							<th>Time</th>
+							<th>First Participant</th>
+							<th>Bets for First Participant</th>
+							<th>Second Participant</th>
+							<th>Bets for Second Participant</th>
+						<thead>
+						<tbody>
+					";
+		while($row = $races->fetch_assoc()) {
+			echo "<tr><td>";
+			echo $index .".";
+			echo "</td>";
+			
+			echo "<td>";
+			echo $row["date"];
+			echo "</td>";
+			
+			echo "<td>";
+			echo $row["time"];
+			echo "</td>";				
+			
+			echo "<td>";
+			echo $row["r1"];
+			echo "</td>";
+			
+			echo "<td>";
+			echo $row["b1"];
+			echo "</td>";
+			
+			echo "<td>";
+			echo $row["r2"];
+			echo "</td>";
+			
+			echo "<td>";
+			echo $row["b2"];
+			echo "</td></tr>";
+			
+			$index = $index + 1;
+		 }
+		 echo "</tbody>
+				</table>";
+	}
+}
+
+$conn->close();
+	}
+}
+?>
